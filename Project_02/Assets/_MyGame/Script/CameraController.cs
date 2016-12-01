48014356b1871c3de998d04b3e098271e5b2f164
ji-supark <t_ieri@naver.com>
2016-12-01 16:49:59 +0900

    test

---
+++ Project_02/Assets/_MyGame/Script/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public float distance = 10.0f; //카메라와 대상의 거리.
+    public float hozAngle = -90.0f; //카메라의 수평 위치.
+    public float verAngle = 45.0f; //카메라의 수직위치.
+    public Vector3 offset = Vector3.zero; //카메라의 위치 수정 값.
+    public Transform lookAtTarget; //카메라가 바라보는 대상.
+    
+	void LateUpdate () {
+        if (lookAtTarget != null)
+        {
+            //카메라 바라보는 위치 설정.
+            Vector3 lookPosition = lookAtTarget.position + offset;
+            //카메라의 상대위치 설정.
+            Vector3 relativePos = Quaternion.Euler(verAngle, hozAngle, 0) *
+                new Vector3(0, 0, -distance);
+            //카메라의 위치를 적용.
+            transform.position = lookPosition + relativePos;
+            //lookAtTarget을 카메라가 바라보게 한다.
+            transform.LookAt(lookAtTarget);
+         }
+	}
+}
---
+++ Project_02/Assets/_MyGame/Script/CharController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharController : MonoBehaviour {
+    public NavMeshAgent agent; //navmeshagent 저장 변수.
+    public Animator anim; //메카님용 animatior 저장 변수
+
+    public GameObject clickEffect;
+
+    public Vector3 destination; //목적지를 저장할 변수.
+
+
+	void Start () {
+        //component 변수에 할당. 
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        //목적지 초기화.
+        destination = agent.destination;
+	}//start
+	
+	void Update () {
+        //마우스왼쪽버튼 누르기
+	    if(Input.GetMouseButtonDown(0))
+        {
+            MakeClickPoint();
+        }
+
+        //설정된 목적지와 나와의 거리가 최소 이동거리보다 큰지 판단.
+        if ( Vector3.Distance(destination, transform.position) > 1.0f)
+        {
+            agent.destination = destination; //이동.
+        }
+
+        //속도의 길이를 판단해서 애니메이션의 상태를 변환시킨다.
+        if (agent.velocity.magnitude > 0.1f) // idle -> run
+            anim.SetFloat("Speed", agent.velocity.magnitude);
+        else
+            anim.SetFloat("Speed", 0.0f); // run -> idle
+
+        //attack 구현(키 조합).
+        if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.LeftShift))
+        {
+            //제자리에 멈춤.
+            agent.destination = transform.position;
+            destination = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            //공격 동작 실행.
+            anim.SetBool("IsAttack", true);
+        }
+        else
+            anim.SetBool("IsAttack", false);
+
+    }//update
+
+    void MakeClickPoint()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //광선을 만든다.
+        RaycastHit hit; 
+
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity, 1<<9))
+        {
+            //목적지 클릭지점으로 생성.
+            destination = new Vector3(hit.point.x,
+                hit.point.y, hit.point.z);
+
+            //해당지점에 클릭이펙트 생성.
+            GameObject insClickEffect = Instantiate(clickEffect,
+                hit.point, Quaternion.identity) as GameObject;
+            Destroy(insClickEffect, 1.0f);
+        }
+    }
+
+}//class
